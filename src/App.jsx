a112db9b4ac16b3f3a71f3ef8e8e6f64d4455370
riyakumar19 <kumarriya227@gmail.com>
2024-07-06 12:21:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      {/* <Projects/> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './projects.css'
+
+const Projects = () => {
+  return (
+    <div>
+      project
+    </div>
+  )
+}
+
+export default Projects
